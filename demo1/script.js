7fe0c79269a4517bd12218235447c468ba01e5cf
dingxiang <dx_root@163.com>
2024-06-27 00:49:34 +0800

    demo1

---
+++ demo1/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aboutSection = document.getElementById("about");
+    aboutSection.addEventListener('click', function(){
+        alert('点击关于我部分！')
+    });
+});
